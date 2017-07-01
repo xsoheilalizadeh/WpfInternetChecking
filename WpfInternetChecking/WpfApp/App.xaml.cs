072a41537886acb7579351c9612528905fc37245
Soheil Alizadeh <Soheil-Alizadeh@outlook.com>
2017-07-01 15:48:27 +0430

    Up Files

---
+++ WpfInternetChecking/WpfApp/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+
+namespace WpfApp
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            try
+            {
+                Dns.GetHostEntry("www.google.com");
+                StartupUri = new Uri("/WpfApp;component/MainWindow.xaml", UriKind.Relative);
+            }
+            catch
+            {
+                StartupUri = new Uri("/WpfApp;component/NotInternetWindow.xaml", UriKind.Relative);
+            }
+        }
+    }
+}
---
+++ WpfInternetChecking/WpfApp/InternetExtention.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace WpfApp
+{
+    public static class InternetExtention
+    {
+        public static bool HasConnection()
+        {
+            try
+            {
+                var i = Dns.GetHostEntry("www.google.com");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
